fd065a8c0ffe0e226d50be67ba16fc71358a67f1
azamind <azaminders@gmail.com>
2022-05-12 15:51:02 +0300

    Add project files.

---
+++ TopShopClient/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TopShopClient
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TopShopClient/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TopShopClient
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
