a730348402db30754cd1d8d8d5b843a738fbb95b
azamind <azaminders@gmail.com>
2022-05-12 16:12:12 +0300

    Model Add

---
+++ TopShopServer/Models/Brand.cs
@@ -0,0 +1,9 @@
+﻿namespace TopShopServer.Models
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ TopShopServer/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace TopShopServer.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public Brand? BrandId { get; set; }
+        public Brand? Brand { get; set; }
+        public string? Title { get; set; }
+        public IEnumerable<Size>? Size { get; set; }
+        public decimal? Price { get; set; }
+        public string? Article { get; set; }
+        public string? Code { get; set; }
+        public string? Description { get; set; }    
+        public string? Photo { get; set; }
+        public DateTime? CreatedAt { get; set; }  
+    }
+}
