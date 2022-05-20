49d56c03118bc4c15e6fab987a2470298242b161
azamind <azaminders@gmail.com>
2022-05-21 00:25:38 +0300

    develop form fields

--- TopShopClient/Pages/Product/CreateEditPage.xaml.cs
+++ TopShopClient/Pages/Product/CreateEditPage.xaml.cs
@@ -1,9 +1,15 @@
+using TopShopClient.ViewModels.Product;
+
 namespace TopShopClient.Pages.Product;
 
 public partial class CreateEditPage : ContentPage
 {
+	private readonly CreateEditViewModel viewModel;
+
 	public CreateEditPage()
 	{
 		InitializeComponent();
+		BindingContext = viewModel = new CreateEditViewModel();
 	}
+
 }
--- TopShopClient/ViewModels/Product/CreateEditViewModel.cs
+++ TopShopClient/ViewModels/Product/CreateEditViewModel.cs
@@ -1,9 +1,55 @@
-﻿namespace TopShopClient.ViewModels.Product
+﻿using System.Windows.Input;
+
+namespace TopShopClient.ViewModels.Product
 {
     public class CreateEditViewModel : BaseViewModel
     {
+        public ICommand PhotoUploadCommand { get; set; }
 
-        
+        public CreateEditViewModel()
+        {
+            PhotoUploadCommand = new Command(async () => await ExecutePhotoUploadCommand());
+        }
 
+        private async Task<FileResult> ExecutePhotoUploadCommand()
+        {
+            try
+            {
+                var FileType = new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+                {
+                    { DevicePlatform.iOS, new[] {"public.product.images"} },
+                    { DevicePlatform.Android, new[] {"product/images"} },
+                    { DevicePlatform.WinUI, new[] {".jpg", ".png"} },
+                    { DevicePlatform.Tizen, new[] {"*/*"} },
+                    { DevicePlatform.macOS, new[] {"jpg", "png"} },
+                });
+                var options = new PickOptions
+                {
+                    PickerTitle = "Please select a product file",
+                    FileTypes = FileType,
+                };
+                var result = await FilePicker.PickAsync(options);
+
+                if(result != null)
+                {
+                    var Text = $"File Name: {result.FileName}";
+                    if(result.FileName.EndsWith("jpg", StringComparison.OrdinalIgnoreCase) ||
+                       result.FileName.EndsWith("jpg", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var stream = await result.OpenReadAsync();
+                        var Image = ImageSource.FromStream(() => stream);
+                    }
+
+                    return result;
+                }
+
+            } 
+            catch (Exception e)
+            {
+
+            }
+
+            return null;
+        }
     }
 }
